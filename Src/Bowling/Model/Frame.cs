f1f6dbe922192c362dfc6d11dffd082dd9293242
Sébastien Parizel <sebastien.parizel@ingenico.com>
2020-02-20 19:46:10 +0100

    Create Frame object

---
+++ Src/Bowling/Model/Frame.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bowling.Model
+{
+    public class Frame
+    {
+        public int FirstLaunch { get; protected set; }
+        public int SecondLaunch { get; protected set; }
+        public bool IsStrike { get; protected set; } = false;
+        public bool IsSpare { get; protected set; } = false;
+
+        protected Frame(int firstLaunch, int secondLaunch)
+        {
+            FirstLaunch = firstLaunch;
+            SecondLaunch = secondLaunch;
+        }
+
+        public static Frame CreateFrame(int firstLaunch, int secondLaunch)
+        {
+            return new Frame(firstLaunch, secondLaunch);
+        }
+    }
+}
---
+++ Test/Bowling.Tests/Model/FrameTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bowling.Model;
+using Xunit;
+
+namespace Bowling.Tests.Model
+{
+    public class FrameTest
+    {
+        [Theory]
+        [InlineData(1, 5, false, false)]
+        [InlineData(2, 7, false, false)]
+        public void ValidateFrameCreation(int firstLaunch, int secondLaunch, bool isSpareExpected, bool isStrikeExpected)
+        {
+            Frame frame = Frame.CreateFrame(firstLaunch, secondLaunch);
+            Assert.Equal(isSpareExpected, frame.IsSpare);
+            Assert.Equal(isStrikeExpected, frame.IsStrike);
+        }
+    }
+}
