0e33582f15fabeed6e253375f1294c9fe8f57cf0
Sébastien Parizel <sebastien.parizel@gmail.com>
2020-02-22 09:43:13 +0100

    Add GameParser + remove unused using

---
+++ Src/Bowling/Builder/GameParser.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Bowling.Model;
+
+namespace Bowling.Builder
+{
+    public class GameParser
+    {
+        private readonly Queue<int> _launches;
+
+        public GameParser(string[] parameters)
+        {
+            _launches = new Queue<int>();
+            InitializeLaunches(parameters);
+        }
+
+        private void InitializeLaunches(string[] parameters)
+        {
+            foreach (var param in parameters)
+            {
+                int launch;
+                if (!Int32.TryParse(param, out launch))
+                {
+                    //TODO
+                }
+                _launches.Enqueue(launch);
+            }
+        }
+
+        public Game BuildGame()
+        {
+            var game = new Game();
+            while (_launches.Any())
+            {
+                var first = _launches.Dequeue();
+                var second = _launches.Dequeue();
+                Frame frame = Frame.CreateFrame(first, second);
+                game.AddFrame(frame);            }
+
+
+            return game;
+        }
+    }
+}
---
+++ Test/Bowling.Tests/Builder/GameParserTest.cs
@@ -0,0 +1,19 @@
+﻿using Bowling.Builder;
+using Bowling.Model;
+using Xunit;
+
+namespace Bowling.Tests.Builder
+{
+    public class GameParserTest
+    {
+        [Theory]
+        [InlineData(2, 7, "1", "1", "3", "2")]
+        public void GivenAValideParameterSet_WhenIPaseIt_ThenIGetAValideGame(int expectedFrameCount, int expectedFinalScore, params string[] parameters)
+        {
+            var builder = new GameParser(parameters);
+            Game game = builder.BuildGame();
+            Assert.Equal(expectedFrameCount, game.Count);
+            //Assert.Equal(expectedFinalScore, game.GetScore());
+        }
+    }
+}
